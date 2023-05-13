9a0a08d3e26f0449fe5dac10efd9afe2beddf996
Brystol <odido160@gmail.com>
2023-05-13 20:30:53 +0200

    need to fix bug with Order

---
+++ src/main/java/payroll/EmployeeNotFoundException.java
@@ -0,0 +1,9 @@
+package payroll;
+
+public class EmployeeNotFoundException extends RuntimeException{
+
+    EmployeeNotFoundException(Long id){
+        super("Could not find employee " + id);
+    }
+
+}
